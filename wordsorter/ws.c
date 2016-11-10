c48e8e6e09fd8664cbd5fa9c1a537111b6340e7f
Daniel Ems <danielems3@outlook.com>
2016-11-10 08:05:51 -0500

    Try and fix seg fault error from malloc while taking in fgets

--- wordsorter/ws.c
+++ wordsorter/ws.c
@@ -12,19 +12,18 @@ int main(int argc, char *argv[])
 {
 
 	int partial_results = 0;
-	char *
-	opterr;
+	opterr = 0;
 
 	int command_arguments;
 	while (-1 < (command_arguments = getopt(argc, argv, "rnlsauhc:"))){
 		switch(command_arguments){
 			case 'r':
 				//TODO: program prints in reverse
-				printf("-r\n");
+				//printf("-r\n");
 				break;
 			case 'n':
 				//TODO: sorts as if they were numbers
-				printf("-n\n");
+				//printf("-n\n");
 				break;
 			case 'l':
 				//TODO: sorts the words by length
@@ -32,19 +31,19 @@ int main(int argc, char *argv[])
 				break;
 			case 's':
 				//TODO: sorts words by scrabble score
-				printf("-s\n");
+				//printf("-s\n");
 				break;
 			case 'a':
 				//TODO: sorts lexicographically
-				printf("-a\n");
+				//printf("-a\n");
 				break;
 			case 'u':
 				//TODO: only prints unique words, case sensitive
-				printf("-u\n");
+				//printf("-u\n");
 				break;
 			case 'h':
 				//TODO: prints brief help message of any normal operation.
-				printf("-h\n");
+				//printf("-h\n");
 				break;
 			case 'c':
 				//TODO: prints number passed results
@@ -52,29 +51,41 @@ int main(int argc, char *argv[])
 				printf(" number of words in list %d\n", partial_results);
 				break; 
 			default:
-				//printf("Default option handler got %c\n", command_arguments);
+				printf("Default option handler got %c\n", command_arguments);
 				break;
 		}
 	}
 
-	char *tmp_hope[256];
-	char tmp_buf[256];
-	char *b;
-	char *c;
-	int i =0;
+
+	size_t last_idx =0;
+	char **tmp_hope = malloc((1+last_idx) * sizeof(tmp_hope));
+	tmp_hope[0] = NULL;
+	char tmp_buf[64];
+	char *set_null;
+	char *token;
+	FILE *first;
 	if(optind != argc){
-		FILE *first = fopen(argv[optind], "r");
-		while(fgets(tmp_buf, sizeof(tmp_buf), first) !=NULL){
-			b = tmp_buf;
-			while(1){
-				c = strtok(b, " \n");
-				b = NULL;
-				if(c ==NULL){
-					break;
-				}
-				tmp_hope[i++] = c;
-				printf("%s\n", c);
+		first = fopen(argv[optind], "r");
+		
+	}
+	while(fgets(tmp_buf, sizeof(tmp_buf), first)){
+		set_null = tmp_buf;
+		while(1){
+			token = strtok(set_null, " \n");
+			set_null = NULL;
+			if(token ==NULL){
+				break;
 			}
+			last_idx++;
+			void *tmp_buffer = realloc(tmp_hope, sizeof(*tmp_hope) * last_idx);
+			tmp_hope = tmp_buffer;
+			tmp_hope[last_idx] = token;
+			printf("%s\n", token);
+			//free(tmp_buffer);
 		}
 	}
+	fclose(first);
+	free(tmp_hope);
+	//printf("%s", tmp_hope);
+
 }
