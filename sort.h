8d8410f09301dba0706c013c8860b8c2e477b330
Daniel Ems <danielems3@outlook.com>
2016-11-11 11:23:57 -0500

    submit final sort.h for submission

--- sort.h
+++ sort.h
@@ -12,9 +12,7 @@ int lexi_sort(const void *a, const void *b);
 
 void print_reverse(char **tmp_hope, int last_idx, int print_limit, bool unique);
 
-//void print_unique(char **tmp_hope, int last_idx, int print_limit);
-
-void print_lexi(char **tmp_hope, int print_limit);
+void print_lexi(char **tmp_hope, int print_limit,int last_idx, bool unique);
 
 #endif
 
