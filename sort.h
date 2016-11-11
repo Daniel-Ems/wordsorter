1d1fb64707c59645f1fb8f05673419a5ba3dc292
Daniel Ems <danielems3@outlook.com>
2016-11-10 21:26:36 -0500

    new header file for ws.c functions

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef sort.h
+#define sort.h
+
+
+void print_unique(char **tmp_hope, int last_idx, int print_limit);
+
+int length_sort(const void *a, const void *b);
+
+int lexi_sort(const void *a, const void *b);
+
+char *get_words(char *token);
