1a0c428d8f5c63444b2b8ccc65f6e4998e6b71a6
Daniel Ems <danielems3@outlook.com>
2016-11-11 10:44:21 -0500

    Add unique to reverse print

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 char *get_words(char *token);
 
@@ -9,9 +10,11 @@ int length_sort(const void *a, const void *b);
 
 int lexi_sort(const void *a, const void *b);
 
-void print_reverse(char **tmp_hope, int last_idx, int print_limit);
+void print_reverse(char **tmp_hope, int last_idx, int print_limit, bool unique);
 
-void print_unique(char **tmp_hope, int last_idx, int print_limit);
+//void print_unique(char **tmp_hope, int last_idx, int print_limit);
+
+void print_lexi(char **tmp_hope, int print_limit);
 
 #endif
 
