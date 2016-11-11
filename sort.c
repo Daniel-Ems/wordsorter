b32230818b9ed823643629a7e24d4186f6e2f6b6
Daniel Ems <danielems3@outlook.com>
2016-11-11 10:43:57 -0500

    Add unique function to reverse print

--- sort.c
+++ sort.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 enum{LONGEST_WORD = 36};
 
@@ -30,12 +31,12 @@ int length_sort(const void *a, const void *b)
 {
 	return strlen(*(const char**) a) - strlen( *(const char**)b);
 }
-
+/*
 // Alexander Dow helped me with solving the inner loop.  
 void print_unique(char **tmp_hope, int last_idx, int print_limit)
 {
 	int b = 0;
-	for(int i = 0; i < print_limit; i++){
+	for(int i = 0; i <= print_limit; i++){
 	int count = 0;
 		for(b=(i+1); b < last_idx; b++){
 			if(!strcmp(tmp_hope[i],tmp_hope[b])){
@@ -47,13 +48,31 @@ void print_unique(char **tmp_hope, int last_idx, int print_limit)
 		}
 	}
 }
-
-void print_reverse(char **tmp_hope, int last_idx, int print_limit)
+*/
+void print_reverse(char **tmp_hope, int last_idx, int print_limit, bool unique)
 {
 	for(int i = (last_idx - 1); i >= last_idx - print_limit; i--){
+		//printf("%s\n", tmp_hope[i]);
+		int count = 0;
+		for(int b=(i - 1); b > 0; b--){
+			if(unique == true && !strcmp(tmp_hope[i],tmp_hope[b])){
+				count++;
+			}
+		}
+		if(count == 0){
+			printf("%s\n", tmp_hope[i]);
+		}
+	}
+}
+
+void print_lexi(char **tmp_hope, int print_limit)
+{
+	for(int i = 0; i < print_limit; i++){
 		printf("%s\n", tmp_hope[i]);
 	}
 }
+
+
 
 
 
