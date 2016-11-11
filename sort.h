3e3c0499b2e489897d2265f9df5ba5a5c3de6adb
Daniel Ems <danielems3@outlook.com>
2016-11-11 08:51:24 -0500

    Reorganize .h page

--- sort.h
+++ sort.h
@@ -3,11 +3,11 @@
 
 #include <stdlib.h>
 
+char *get_words(char *token);
+
 int length_sort(const void *a, const void *b);
 
 int lexi_sort(const void *a, const void *b);
-
-char *get_words(char *token);
 
 void print_reverse(char **tmp_hope, int last_idx, int print_limit);
 
