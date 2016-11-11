bf73d1e419448b2be8152628f0372c7e866abfc1
Daniel Ems <danielems3@outlook.com>
2016-11-11 00:08:23 -0500

    Add to Makefile to make program

--- sort.h
+++ sort.h
@@ -1,11 +1,17 @@
-#ifndef sort.h
-#define sort.h
+#ifndef SORT_H
+#define SORT_H
 
-
-void print_unique(char **tmp_hope, int last_idx, int print_limit);
+#include <stdlib.h>
 
 int length_sort(const void *a, const void *b);
 
 int lexi_sort(const void *a, const void *b);
 
 char *get_words(char *token);
+
+void print_reverse(char **tmp_hope, int last_idx, int print_limit);
+
+void print_unique(char **tmp_hope, int last_idx, int print_limit);
+
+#endif
+
